ac65e74dbed1caeb853f8ee07c23081056130d94
Patrick Brito <patrickbrito95@gmail.com>
2023-01-05 10:33:12 -0300

    feat: struture mvp done

--- src/Sections/Footer/index.jsx
+++ src/Sections/Footer/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './styles.css';
+import MarvelLogoImg from '../../Assets/marvel-logo.png'
+import Icon from '../../Components/Icon';
 
 const Footer = () => {
     return(
@@ -23,7 +25,55 @@ const Footer = () => {
                 </div>
             </div>
             <div className='wrapper-footer'>
-
+                <div className="first-block">
+                    <div className='marvel-logo'>
+                        <img width={50} height={67} src={MarvelLogoImg} alt="marvel logo" />
+                    </div>
+                    <div className='about-marvel'>
+                        <span>A Marvel</span>
+                        <div className='footer-links'>
+                            <a>Sobre a Marvel</a>
+                            <a>Ajuda</a>
+                            <a>Carreiras</a>
+                        </div>
+                    </div>
+                    <div className='media-marvel'>
+                    <span>Mídia</span>
+                        <div className='footer-links'>
+                            <a>Filmes</a>
+                            <a>Quadrinhos</a>
+                            <a>Programas de TV</a>
+                            <a>Jogos</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='second-block'>
+                    <div className='last-news-blog'>
+                    <span>Última do blog</span>
+                        <div className='headline-news'>
+                            <img width={60} height={60} src="https://pittnews.com/wp-content/uploads/2022/09/O_Marvel_LaMarca_AP-900x600.jpg" />
+                            <p>Vingadores confirma grande teoria dos fãs sobre a Marvel...</p>
+                        </div>
+                        <a>Continuar lendo</a>
+                    </div>
+                    <div className='follow-us'>
+                        <span>Siga a Marvel</span>
+                        <div className='social-icons'>
+                            <a>
+                                <Icon name="facebook" />
+                            </a>
+                            <a>
+                                <Icon name="spotify" />
+                            </a>
+                            <a>
+                                <Icon name="instagram" />
+                            </a>
+                            <a>
+                                <Icon name="youtube" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
