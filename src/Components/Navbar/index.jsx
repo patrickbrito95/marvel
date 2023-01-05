c1dff82c356e653552b8ad52be111e6859462016
Patrick Brito <patrickbrito95@gmail.com>
2023-01-05 13:49:51 -0300

    style: implement responsivity

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,30 +1,66 @@
-import react from 'react';
-import NavbarItem from '../NavbarItem';
-import './style.css'
+import React, { useState } from "react";
+import { useMediaQuery } from "react-responsive";
+import NavbarItem from "../NavbarItem";
+import "./style.css";
+import HamburguerButton from "../../Assets/hamburguer.png";
 
 const Navbar = () => {
+  const itens = [
+    { name: "FILMES", path: "/movies" },
+    { name: "QUADRINHOS", path: "/hq" },
+    { name: "PROGRAMAS DE TV", path: "/tv" },
+    { name: "JOGOS", path: "/games" },
+  ];
 
-    const itens = [
-        { "name": "FILMES", "path": "/movies" },
-        { "name": "QUADRINHOS", "path": "/hq" },
-        { "name": "PROGRAMAS DE TV", "path": "/tv" },
-        { "name": "JOGOS", "path": "/games" }
-    ];
+  const isDesktop = useMediaQuery({ minWidth: 991 });
 
+  const [isOpen, setIsOpen] = useState(false);
 
-    return(
-        <div className='navbar'>
+  return (
+    <>
+      {isDesktop ? (
+        <div className="navbar">
+          <div className="logo">
+            <img
+              width={140}
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/2560px-Marvel_Logo.svg.png"
+            />
+          </div>
+          <div className="navbar-item">
+            {itens.map((item) => (
+              <NavbarItem name={item.name} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className="navbar">
             <div className="logo">
-                <img width={140} src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/2560px-Marvel_Logo.svg.png" />
+              <img
+                width={140}
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Marvel_Logo.svg/2560px-Marvel_Logo.svg.png"
+              />
             </div>
             <div className="navbar-item">
-                {itens.map((item) => (
-                    <NavbarItem name={item.name} />
-                     
-                ))}
+              <button
+                className="dropdown-button"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                <img src={HamburguerButton} />
+              </button>
             </div>
-        </div>
-    )
-}
+          </div>
+          {isOpen ? (
+            <div className="dropdown">
+              {itens.map((item) => (
+                <NavbarItem name={item.name} />
+              ))}
+            </div>
+          ) : null}
+        </>
+      )}
+    </>
+  );
+};
 
 export default Navbar;
--- src/Sections/CardsFanMarvel/index.jsx
+++ src/Sections/CardsFanMarvel/index.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import './styles.css';
-import CardsFanMarvelImg from '../../Assets/cards-fan-desktop.png'
+import React from "react";
+import "./styles.css";
+import CardsFanMarvelImg from "../../Assets/cards-fan-desktop.png";
 
 const CardsFanMarvel = () => {
-    return(
-        <div className='wrapper-cards-fan'>
-            <h1 className='title-cards-fan'>MARVEL MASTERCARD</h1>
-            <h4 className='subtitle-cards-fan'>Desbloqueie seus super-poderes</h4>
-            <img className='card-marvel-image' src={CardsFanMarvelImg} alt="Cartões da Marvel" />
-            <div className='wrapper-buttons'>
-                <button className='call-to-action'>CADASTRE-SE AGORA</button>
-                <button className='more-information'>ENTENDA COMO FUNCIONA</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1 className="title-cards-fan">MARVEL MASTERCARD</h1>
+      <h4 className="subtitle-cards-fan">Desbloqueie seus super-poderes</h4>
+      <img
+        className="card-marvel-image"
+        src={CardsFanMarvelImg}
+        alt="Cartões da Marvel"
+      />
+      <div className="wrapper-buttons">
+        <button className="call-to-action">CADASTRE-SE AGORA</button>
+        <button className="more-information">ENTENDA COMO FUNCIONA</button>
+      </div>
+    </div>
+  );
+};
 
 export default CardsFanMarvel;
